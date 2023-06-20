45088c528fd53ef85561c332307462cc08906840
xumingyu2018 <260602448@qq.com>
2023-06-21 00:01:16 +0800

    路由配置

---
+++ src/router/index.ts
@@ -0,0 +1,21 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/main'
+    },
+    {
+      path: '/login',
+      component: () => import('../views/login/login.vue')
+    },
+    {
+      path: '/main',
+      component: () => import('../views/main/main.vue')
+    }
+  ]
+})
+
+export default router
