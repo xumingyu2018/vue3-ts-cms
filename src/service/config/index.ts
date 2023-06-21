d9e447324c6a4cbc5209b131dcf48e671838ab7c
xumingyu2018 <260602448@qq.com>
2023-06-21 18:41:30 +0800

    区分开发环境和生产环境方法一

--- src/service/config/index.ts
+++ src/service/config/index.ts
@@ -1,2 +1,16 @@
-export const BASE_URL1 = 'http://152.136.185.210:5000'
-export const TIME_OUT1 = 10000
+// export const BASE_URL1 = 'http://152.136.185.210:5000'
+// export const TIME_OUT1 = 10000
+
+console.log(import.meta.env.DEV);
+console.log(import.meta.env.PROD);
+console.log(import.meta.env.SSR);
+
+let BASE_URL1 = ''
+if(import.meta.env.PROD) {
+    BASE_URL1 = 'http://codercba.prod:8000'
+} else {
+    BASE_URL1 = 'http://codercba.dev:8000'
+}
+
+export const TIME_OUT = 10000
+export { BASE_URL1 }
