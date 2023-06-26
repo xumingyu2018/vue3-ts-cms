4052a0cfe819bcfe79b147171fc76558d5386995
xumingyu2018 <260602448@qq.com>
2023-06-26 16:02:43 +0800

    ElementPlus按需引入项目

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,8 @@ import 'normalize.css'
 import router from './router'
 import pinia from './store'
 
+// 全局注册element-plus
+// import ElementPlus from 'element-plus'
+// import 'element-plus/dist/index.css' 
+
 createApp(App).use(router).use(pinia).mount('#app')
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,17 +1,26 @@
 <template>
   <div class="login">
     <h2>login</h2>
+    <el-row class="mb-4">
+      <el-button plain>Plain</el-button>
+      <el-button type="primary" plain>Primary</el-button>
+      <el-button type="success" plain>Success</el-button>
+      <el-button type="info" plain>Info</el-button>
+      <el-button type="warning" plain>Warning</el-button>
+      <el-button plain>Danger</el-button>
+    </el-row>
   </div>
 </template>
 
 <script setup lang="ts">
-import hyRequest from '@/service';
-console.log("-----");
-hyRequest.get({
-  url: '/home/multidata'
-}).then((res) => {
-  console.log(res);
-})
+// import hyRequest from '@/service';
+// console.log("-----");
+// hyRequest.get({
+//   url: '/home/multidata'
+// }).then((res) => {
+//   console.log(res);
+// })
+
 </script>
 
 <style lang="less" scoped>
