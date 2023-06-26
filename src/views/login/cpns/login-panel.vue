2382e5c9fe0fe3e5fb00e6ea8ff6be60c90d8414
xumingyu2018 <260602448@qq.com>
2023-06-26 19:40:33 +0800

    实现tabs插槽插入图标icon

--- src/views/login/cpns/login-panel.vue
+++ src/views/login/cpns/login-panel.vue
@@ -6,6 +6,12 @@
     <div class="tabs">
       <el-tabs type="border-card" stretch="true">
         <el-tab-pane label="账号登录">
+          <template #label>
+            <div class="label">
+              <el-icon><UserFilled /></el-icon>
+              <span class="text">账号登录</span>
+            </div>
+          </template>
           <el-form>
             <el-form-item label="账号">
               <el-input placeholder="请输入账号"></el-input>
@@ -15,12 +21,21 @@
             </el-form-item>
           </el-form>
         </el-tab-pane>
-        <el-tab-pane label="手机登录">Config</el-tab-pane>
+        <el-tab-pane label="手机登录">
+          <template #label>
+            <div class="label">
+              <el-icon><Cellphone /></el-icon>
+              <div class="text">手机登录</div>
+            </div>
+          </template>
+          <div>手机登录</div>
+          <div>验证码</div>
+        </el-tab-pane>
       </el-tabs>
     </div>
     <!-- 底部区域 -->
     <div class="controls">
-      <el-checkbox v-model="isRemPwd" label="记住密码" size="large "></el-checkbox>
+      <el-checkbox v-model="isRemPwd" label="记住密码" size="large"></el-checkbox>
       <el-link type="primary" href="">忘记密码</el-link>
     </div>
     <el-button class="login-btn" type="primary" size="large">立即登录</el-button>
@@ -39,6 +54,14 @@ const isRemPwd = ref(false)
 
   .title {
     text-align: center;
+  }
+
+  .label {
+    display: flex;
+
+    .text {
+      margin-left: 5px;
+    }
   }
 
   .controls {
