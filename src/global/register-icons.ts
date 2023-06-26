b5d47c63004c65f0e3a2f464c671d2f6be2abae2
xumingyu2018 <260602448@qq.com>
2023-06-26 17:48:09 +0800

    注册全局图标

---
+++ src/global/register-icons.ts
@@ -0,0 +1,11 @@
+// 图标的全局注册
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import type { App } from 'vue'
+
+function registerIcons(app: App) {
+  for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+}
+
+export default registerIcons
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import App from './App.vue'
 import 'normalize.css'
 import router from './router'
 import pinia from './store'
+import registerIcons from './global/register-icons'
 
 // 全局注册element-plus
 // import ElementPlus from 'element-plus'
-// import 'element-plus/dist/index.css' 
+// import 'element-plus/dist/index.css'
 
-createApp(App).use(router).use(pinia).mount('#app')
+createApp(App).use(router).use(pinia).use(registerIcons).mount('#app')
