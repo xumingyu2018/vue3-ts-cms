0307b2f8ffc10fe90502f669dd0830819b734019
xumingyu2018 <260602448@qq.com>
2023-06-27 11:06:24 +0800

    实现表单验证和消息提示框

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,13 @@ import router from './router'
 import pinia from './store'
 import registerIcons from './global/register-icons'
 
+// 针对ElMessage和ElLoading等组件引入样式(手动按需引入)
+import 'element-plus/theme-chalk/el-message.css'     
+
 // 全局注册element-plus
 // import ElementPlus from 'element-plus'
 // import 'element-plus/dist/index.css'
+
+// 安装vite-plugin-style-import/consola并配置vite.config.ts（自动按需引入样式）
 
 createApp(App).use(router).use(pinia).use(registerIcons).mount('#app')
--- src/views/login/cpns/login-panel.vue
+++ src/views/login/cpns/login-panel.vue
@@ -44,7 +44,7 @@
 </template>
 
 <script setup lang="ts">
-import { reactive, ref } from 'vue'
+import { ref } from 'vue'
 import paneAccount from './pane-account.vue'
 
 const activeName = ref('account')
