3202b2f8a75c5a3fce588bf7a47a3ad7432220b4
xumingyu2018 <260602448@qq.com>
2023-06-28 09:51:13 +0800

    router跳转main页面

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -2,6 +2,7 @@ import { defineStore } from "pinia";
 import { accountLoginRequest } from '@/service/login/login'
 import type { IAccount } from "@/types";
 import { localCache } from "@/utils/cache";
+import router from "@/router";
 
 const useLoginStore = defineStore('login', {
     state: () => ({
@@ -14,14 +15,17 @@ const useLoginStore = defineStore('login', {
     actions: {
         async loginAccountAction(account: IAccount) {
             // 1. 帐号登录，axios获取token等信息
-            const loginResult = await accountLoginRequest(account)
-            this.id = loginResult.data.id
-            this.token = loginResult.data.token
-            this.username = loginResult.data.username
+            // const loginResult = await accountLoginRequest(account)
+            // this.id = loginResult.data.id
+            // this.token = loginResult.data.token
+            // this.username = loginResult.data.username
 
             // 2. 进行本地缓存（使用封装的cache工具）
             // localStorage.setItem('token', this.token)
             localCache.setCache('token', this.token)
+
+            // 3. 页面跳转
+            router.push('/main')
         }
     }
 })
