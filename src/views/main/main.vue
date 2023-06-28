5c40a558c0a73994dae4e18b42e04def3197a60d
xumingyu2018 <260602448@qq.com>
2023-06-28 09:42:37 +0800

    退出登录（删除缓存）

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="main">
     <h2>main</h2>
+    <button @click="handleExitClick">退出登录</button>
   </div>
 </template>
 
 <script setup lang="ts">
+import { localCache } from '@/utils/cache';
+import { useRouter } from 'vue-router';
 
+const router = useRouter()
+const handleExitClick = () => {
+  localCache.deleteCache('token')
+  router.push('/login')
+}
 </script>
 
 <style lang="less" scoped>
