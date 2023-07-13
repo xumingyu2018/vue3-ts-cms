705dfd32df65f00d8039d884b1a196e464afcad2
xumingyu2018 <260602448@qq.com>
2023-07-13 20:54:47 +0800

    使用el-container布局容器搭建main页面整体布局

--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="main">
-    <h2>main</h2>
-    <button @click="handleExitClick">退出登录</button>
+    <el-container class="main-content">
+      <el-aside width="200px">Aside</el-aside>
+      <el-container>
+        <el-header height="50px">Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
@@ -17,5 +22,24 @@ const handleExitClick = () => {
 </script>
 
 <style lang="less" scoped>
+.main {
+  color: red;
+  height: 100%;
 
+  .main-content {
+    height: 100%;
+    .el-aside {
+      background: yellowgreen;
+    }
+
+    .el-header {
+      background: skyblue;
+    }
+
+    .el-main {
+      background: pink;
+    }
+
+  }
+}
 </style>
