9c1b8efb252331938fae3e547303e1c0e2e93fdc
xumingyu2018 <260602448@qq.com>
2023-07-13 20:43:51 +0800

    补充

--- src/service/login/login.ts
+++ src/service/login/login.ts
@@ -9,17 +9,19 @@ export function accountLogin(account: IAccount) {
     })
 }
 
- export function getUserInfoById(id: number) {
+// 获取用户详细信息
+export function getUserInfoById(id: number) {
     return hyRequest.get({
         url: `/users/${id}`,
         // headers: {
         //     Authorization: 'Bearer ' + localCache.getCache('token')
         // }
     })
- }
+}
 
- export function getUserMenusByRoleId(id: number) {
+// 获取角色权限信息
+export function getUserMenusByRoleId(id: number) {
     return hyRequest.get({
         url: `/role/${id}/menu`,
     })
- }
+}
