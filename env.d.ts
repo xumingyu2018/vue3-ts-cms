ed3f28837925e586e8480bf047418f1ddefb8a09
xumingyu2018 <260602448@qq.com>
2023-07-20 11:02:24 +0800

    对日期选择器组件实现国际化

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+declare module '*.vue' {
+    import { DefineComponent } from 'vue'
+    const component: DefineComponent
+    export default component
+}
+
+// 声明国际化配置文件mjs
+declare module '*.mjs'
--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="app">
-    <router-view />
-  </div>
+    <div class="app">
+      <!-- 国际化配置（将组件中的英文转为中文->日期组件） -->
+      <el-config-provider :locale="zhCn">
+        <router-view />
+      </el-config-provider>
+    </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+</script>
 
 <style scoped>
 .app {
