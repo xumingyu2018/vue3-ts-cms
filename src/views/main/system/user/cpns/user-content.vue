1f7337274c58112ad8aff1c1431cf246109ec440
xumingyu2018 <260602448@qq.com>
2023-07-25 15:19:27 +0800

    使用el-table初步搭建表格数据页面

--- src/views/main/system/user/cpns/user-content.vue
+++ src/views/main/system/user/cpns/user-content.vue
@@ -5,11 +5,24 @@
         <el-button type="primary">新建用户</el-button>
       </div>
       <div class="table">
-        <ul>
-            <li v-for="item in usersList" :key="item.id">
-                {{ item.name }}
-            </li>
-        </ul>
+        <el-table :data="usersList" border style="width: 100%">
+            <!-- 勾选框 -->
+            <el-table-column align="center" type="selection" width="50px"/>
+            <!-- 序号 -->
+            <el-table-column align="center" type="index" label="序号" width="60px"/>
+
+            <el-table-column align="center" prop="name" label="用户名" width="150px" />
+            <el-table-column align="center" prop="realname" label="真实姓名" width="150px"/>
+            <el-table-column align="center" prop="cellphone" label="电话号码" width="150px"/>
+            <el-table-column align="center" prop="enable" label="状态" width="100px"/>
+            <el-table-column align="center" prop="createAt" label="创建时间" />
+            <el-table-column align="center" prop="updateAt" label="更新时间" />
+
+            <el-table-column align="center" label="操作" width="150px">
+                <el-button type="primary" icon="Edit" size="small" text>编辑</el-button>
+                <el-button type="danger " icon="Delete" size="small" text>删除</el-button>
+            </el-table-column>
+        </el-table>
       </div>
       <div class="pagination">分页</div>
     </div>
@@ -25,6 +38,7 @@ systemStore.getUsersListAction()
 
 // 2.获取usersList数据(响应式数据：上面代码是异步的，当数据还没拿到是，这一句不起作用，需要用storeToRefs)
 const { usersList } = storeToRefs(systemStore)
+console.log(usersList);
 </script>
 
 <style lang="less" scoped>
@@ -44,4 +58,16 @@ const { usersList } = storeToRefs(systemStore)
         font-size: 22px;
     }
 }
+
+.table {
+    :deep(.el-table__cell) {
+        padding: 12px 0;
+    }
+
+    .el-button {
+        margin-left: 0;
+        padding: 5px 8px;
+    }
+}
+
 </style>
