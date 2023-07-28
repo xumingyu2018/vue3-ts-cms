2b5b136c0d35eed07fc3012eadb630e5e98f47da
xumingyu2018 <260602448@qq.com>
2023-07-28 11:15:23 +0800

    搭建模态框里的表单布局

--- src/views/main/system/user/cpns/user-modal.vue
+++ src/views/main/system/user/cpns/user-modal.vue
@@ -1,9 +1,28 @@
 <template>
   <div class="modal">
     <el-dialog v-model="dialogVisible" title="新建用户" width="30%" center>
-    <span>
-      新建用户
-    </span>
+    <div class="form">
+        <el-form :model="formData" label-width="80px" size="large">
+            <el-form-item label="用户名" prop="name">
+                <el-input v-model="formData.name" placeholder="请输入用户名"></el-input>
+            </el-form-item>
+            <el-form-item label="真实姓名" prop="realname">
+                <el-input v-model="formData.realname" placeholder="请输入真实姓名"></el-input>
+            </el-form-item>
+            <el-form-item label="密码" prop="password">
+                <el-input v-model="formData.password" placeholder="请输入密码" show-password></el-input>
+            </el-form-item>
+            <el-form-item label="电话号码" prop="cellphone">
+                <el-input v-model="formData.cellphone" placeholder="请输入电话号码"></el-input>
+            </el-form-item>
+            <el-form-item label="选择角色" prop="roleId">
+                <el-select v-model="formData.roleId" placeholder="请选择角色"></el-select>
+            </el-form-item>
+            <el-form-item label="选择部门" prop="departmentId">
+                <el-select v-model="formData.departmentId" placeholder="请选择部门"></el-select>
+            </el-form-item>
+        </el-form>
+    </div>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">取消</el-button>
@@ -15,9 +34,17 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 
 const dialogVisible = ref(false)
+const formData = reactive({
+    name: '',
+    realname: '',
+    password: '',
+    cellphone: '',
+    roleId: '',
+    departmentId: ''
+})
 
 function setDialogVisible() {
   dialogVisible.value = true
@@ -29,5 +56,7 @@ defineExpose({
 </script>
 
 <style lang="less" scoped>
-
+.form {
+    padding: 0 20px;
+}
 </style>
