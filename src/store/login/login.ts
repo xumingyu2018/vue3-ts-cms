d4beace0a1b5d84431fac582845f0a16c648521d
xumingyu2018 <260602448@qq.com>
2023-07-28 14:58:08 +0800

    封装角色和部门的请求数据

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -4,6 +4,7 @@ import type { IAccount } from "@/types";
 import { localCache } from "@/utils/cache";
 import { mapMenuToRoutes } from "@/utils/map-menus";
 import router from "@/router";
+import useMainStore from "../main/main";
 
 interface ILoginState {
     token: string,
@@ -41,7 +42,11 @@ const useLoginStore = defineStore('login', {
 
             // 5.进行用户权限信息缓存
             localCache.setCache('userInfo', this.userInfo)
-            localCache.setCache('userMenus', this.userMenus)
+            localCache.setCache('userMenus', this.userMenus) 
+
+            // 6.请求所有roles/department数据(刷新时数据会消失)
+            const mainStore = useMainStore()
+            mainStore.fetchEntireDataAction()
 
             //  页面跳转
             router.push('/main')
@@ -49,7 +54,7 @@ const useLoginStore = defineStore('login', {
 
         // 解决刷新页面后，动态路由丢失的问题(不再是loginAccountAction登录时才执行)
         loadLocalDataAction() {  
-            // 用户进行刷新时默认加载数据
+            // 1.用户进行刷新时默认加载数据
             const token = localCache.getCache('token')
             const userInfo = localCache.getCache('userInfo')
             const userMenus = localCache.getCache('userMenus')
@@ -58,7 +63,12 @@ const useLoginStore = defineStore('login', {
                 this.token = token
                 this.userInfo = userInfo
                 this.userMenus = userMenus
-                // 6.根据菜单menu动态加载路由(使用工具类map-menus.ts)
+
+                // 请求所有roles/department数据(刷新时数据不会消失，经常变化，不放缓存，刷新时请求最新的)
+                const mainStore = useMainStore()
+                mainStore.fetchEntireDataAction()
+
+                // 2.根据菜单menu动态加载路由(使用工具类map-menus.ts)
                 const routes = mapMenuToRoutes(userMenus)
                 // 这里的'main'是根据路由里的name属性来的
                 routes.forEach(route => router.addRoute('main', route))
