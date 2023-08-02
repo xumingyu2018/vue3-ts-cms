eccb64f08921851a9a57af30c983aa2da0059ae8
xumingyu2018 <260602448@qq.com>
2023-08-02 21:22:08 +0800

    增加注释

--- src/service/main/system/system.ts
+++ src/service/main/system/system.ts
@@ -32,29 +32,30 @@ export function editUserData(id: number, userInfo: any) {
 }
 
 /** 抽取公共页面接口 */
+// 查询
 export function getPageListData(pageName: string, queryInfo: any) {
     return hyRequest.post({
       url: `/${pageName}/list`,
       data: queryInfo
     })
-  }
-  
-  export function deletePageData(pageName: string, id: number) {
+}
+// 删除
+export function deletePageData(pageName: string, id: number) {
     return hyRequest.delete({
       url: `/${pageName}/${id}`
     })
-  }
-  
-  export function newPageData(pageName: string, dataInfo: any) {
-    return hyRequest.post({
-      url: `/${pageName}`,
-      data: dataInfo
-    })
-  }
-  
-  export function editPageData(pageName: string, id: number, dataInfo: any) {
-    return hyRequest.patch({
-      url: `/${pageName}/${id}`,
-      data: dataInfo
-    })
-  }
+}
+// 新增
+export function newPageData(pageName: string, dataInfo: any) {
+  return hyRequest.post({
+    url: `/${pageName}`,
+    data: dataInfo
+  })
+}
+// 修改
+export function editPageData(pageName: string, id: number, dataInfo: any) {
+  return hyRequest.patch({
+    url: `/${pageName}/${id}`,
+    data: dataInfo
+  })
+}
