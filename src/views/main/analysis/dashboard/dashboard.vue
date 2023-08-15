9f85f10b44289c3a1787e8f31432991eeb620144
xumingyu2018 <260602448@qq.com>
2023-08-15 10:12:30 +0800

    封装网络请求实现顶部卡片组件动态页面

--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="dashboard">
     <el-row :gutter="10">
-      <template v-for="item in 4" :key="item">
+      <template v-for="item in amountList" :key="item.amount">
         <el-col :span="6">
-          <count-card />
+          <count-card v-bind="item"/>
         </el-col>
       </template>
     </el-row>
@@ -12,6 +12,15 @@
 
 <script setup lang="ts">
 import countCard from "./cpns/count-card/count-card.vue";
+import useAnalysisStore from '@/store/main/analysis/analysis'
+import { storeToRefs } from 'pinia'
+
+// 发起actions封装的网络请求
+const analysisStore = useAnalysisStore()
+analysisStore.fetchAnalysisDataAction()
+
+// 获取数据
+const { amountList } = storeToRefs(analysisStore)
 </script>
 
 <style lang="less" scoped>
